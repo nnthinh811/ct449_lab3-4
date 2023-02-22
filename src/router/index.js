0143c0daef042eafcc2308390756e41c6c4cd73c
Thinh Nguyen <thinhb1910302@student.ctu.edu.vn>
2023-02-22 18:31:46 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,24 @@ import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
 
 const routes = [
+	{
+		path: "/",
+		name: "contactbook",
+		component: ContactBook,
+	},
 
 	{
 		path: "/:pathMatch(.*)*",
 		name: "notfound",
 		component: () => import("@/views/NotFound.vue"),
 		},
+
+	{
+		path: "/contacts/:id",
+		name: "contact.edit",
+		component: () => import("@/views/ContactEdit.vue"),
+		props: true // Truyền các biến trong $route.params vào làm props
+	},
 
 ];
 
